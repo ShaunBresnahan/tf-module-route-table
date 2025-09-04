bf8487102e6df778f2bad9db166718bc68cba399
Shaun Bresnahan <shedman28@googlemail.com>
2025-09-04 18:37:48 +0100

    sdsds

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "route_table_name" {
   description = "The name of the Azure Route Table"
   value       = azurerm_route_table.main.name
 }
+
+output "route_table_id" {
+  description = "The ID of the Azure Route Table"
+  value       = azurerm_route_table.main.id
+}
